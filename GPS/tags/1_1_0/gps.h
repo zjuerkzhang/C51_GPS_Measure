81774b179ebaa7bbc3241576dbc8d21864c9190d
zjuerkzhang <zjuerkzhang@09bfffbe-ea3f-11de-9229-05bbbb8527c3>
2012-04-23 14:06:13 +0000

    Original code

---
+++ GPS/tags/1_1_0/gps.h
@@ -0,0 +1,9 @@
+#ifndef _gps_h_
+#define _gps_h_
+void DataSend(unsigned char SendChar[]);
+void DataSend_char(unsigned char SendChar);
+void UTC2BeiJingTime();
+void CloseGpsPower() ;
+void OpenGpsPower()	;
+void gps_init();
+#endif
---
+++ GPS/tags/1_1_0/keyfuc.h
@@ -0,0 +1,6 @@
+ #ifndef _keyfuc_h_
+#define _keyfuc_h_
+
+unsigned char keyscan();
+void KeyOperate();
+#endif
